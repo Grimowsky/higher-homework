26897a63cbf1aa83c0c59c1e7935092da7af395f
rafalmirkowski@gmail.com <rafalmirkowski@gmail.com>
2019-06-20 15:04:09 +0200

    Updates

---
+++ precache-manifest.6089963fa64bb2a0b98141cc2d639d3e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b6435d75b2af82fff39ab1a5c83526cb",
+    "url": "/higher-homework/index.html"
+  },
+  {
+    "revision": "24107c01ba34c6e0da94",
+    "url": "/higher-homework/static/css/main.fa29f9aa.chunk.css"
+  },
+  {
+    "revision": "681b51057d74cfa8975d",
+    "url": "/higher-homework/static/js/2.65aa1cca.chunk.js"
+  },
+  {
+    "revision": "24107c01ba34c6e0da94",
+    "url": "/higher-homework/static/js/main.361e1f6d.chunk.js"
+  },
+  {
+    "revision": "5a494a943c67d138433f",
+    "url": "/higher-homework/static/js/runtime~main.9a7c8c8d.js"
+  }
+]);
